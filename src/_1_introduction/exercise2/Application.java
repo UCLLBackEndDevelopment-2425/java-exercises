fa534e9b2beaae0a2cc1ad2f1349edcc33a92ca4
Jeroen Rombouts <jeroen.rombouts@ucll.be>
2025-02-11 23:13:50 +0100

    solution ex 1.2

--- src/_1_introduction/exercise2/Application.java
+++ src/_1_introduction/exercise2/Application.java
@@ -3,6 +3,18 @@ package _1_introduction.exercise2;
 public class Application {
 
     public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
 
+        System.out.println("Enter a number: ");
+        double number1 = scanner.nextDouble();
+        System.out.println("Enter a second number: ");
+        double number2 = scanner.nextDouble();
+        System.out.println("Enter a third number: ");
+        double number3 = scanner.nextDouble();
+        double sum = number1 + number2 + number3;
+        double average = sum / 3;
+
+        System.out.println("The sum of the numbers is: " + sum);
+        System.out.println("The average is " + average);
     }
 }
